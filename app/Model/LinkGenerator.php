d79fa869e91e19e9db28a578f716e2521c5a9755
Quaky <quaky@beastblock.cz>
2022-04-30 17:05:31 +0200

    Fix links in AdminMenu

---
+++ app/Model/LinkGenerator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Model;
+
+use Contributte\MenuControl\IMenuItem;
+use Contributte\MenuControl\LinkGenerator\ILinkGenerator;
+use Contributte\MenuControl\LinkGenerator\NetteLinkGenerator;
+
+class LinkGenerator implements ILinkGenerator
+{
+
+    /**
+     * @var \Nette\Application\LinkGenerator
+     */
+    private $linkGenerator;
+
+    public function __construct
+    (
+        $baseUrl,
+        \Nette\Application\LinkGenerator $linkGenerator
+    )
+    {
+        bdump($baseUrl);
+        $this->linkGenerator = $linkGenerator->withReferenceUrl($baseUrl);
+    }
+
+    public function link(IMenuItem $item): string
+    {
+        $action = $item->getAction();
+        if ($action !== null) {
+            return $this->linkGenerator->link($action, $item->getActionParameters());
+        }
+
+        $link = $item->getLink();
+        if ($link !== null) {
+            return $link;
+        }
+
+        return '#';
+    }
+}
