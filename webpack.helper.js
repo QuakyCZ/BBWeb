9767b1de033df551a32ba4193833e449f6139fd2
Quaky <quaky@beastblock.cz>
2022-05-04 22:41:39 +0200

    Update Composer, Bootstrap, Webpack, Node,...

--- webpack.helper.js
+++ webpack.helper.js
@@ -8,11 +8,12 @@ const scriptsGroups = (APP_DIR) => {
             list: [
                 APP_DIR + "/node_modules/jquery/dist/jquery.min.js",
                 APP_DIR + "/node_modules/jquery-ui-sortable/jquery-ui.min.js",
+                APP_DIR + "/www/libs/fontawesome/fontawesome.min.js",
+                APP_DIR + "/node_modules/bootstrap/dist/js/bootstrap.min.js",
                 APP_DIR + "/node_modules/daterangepicker/daterangepicker.js",
                 APP_DIR + "/node_modules/nette.ajax.js/nette.ajax.js",
                 APP_DIR + "/node_modules/live-form-validation/live-form-validation.js",
                 APP_DIR + "/node_modules/nette.ajax.js/extensions/spinner.ajax.js",
-                APP_DIR + "/www/libs/bootstrap/bootstrap.bundle.min.js",
                 // scripty definované zde by se měly objevit i ve webpack.config.js v sekci "externals"
             ]
         },
@@ -20,6 +21,9 @@ const scriptsGroups = (APP_DIR) => {
             dest: 'scriptsAdmin.bundle.js',
             list: [
                 APP_DIR + "/node_modules/jquery/dist/jquery.min.js",
+                APP_DIR + "/node_modules/jquery-ui-sortable/jquery-ui.min.js",
+                APP_DIR + "/www/libs/fontawesome/fontawesome.min.js",
+                APP_DIR + "/node_modules/bootstrap/dist/js/bootstrap.min.js",
                 APP_DIR + "/node_modules/jquery-ui-sortable/jquery-ui.min.js",
                 APP_DIR + "/node_modules/daterangepicker/daterangepicker.js",
                 APP_DIR + "/node_modules/nette.ajax.js/nette.ajax.js",
@@ -29,7 +33,7 @@ const scriptsGroups = (APP_DIR) => {
                 APP_DIR + "/node_modules/ublaboo-datagrid/assets/datagrid.js",
                 APP_DIR + "/node_modules/ublaboo-datagrid/assets/datagrid-spinners.js",
                 APP_DIR + "/node_modules/ublaboo-datagrid/assets/datagrid-instant-url-refresh.js",
-                APP_DIR + "/www/libs/bootstrap/bootstrap.bundle.min.js",
+                APP_DIR + "/www/js/admin/admin.js"
             ]
         }
     ];
