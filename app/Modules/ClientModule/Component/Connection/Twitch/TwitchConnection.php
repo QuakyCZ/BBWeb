c7716212f96f774c40c990befc768a64778aa555
Quaky <quaky@beastblock.cz>
2023-07-18 21:46:56 +0200

    CU-862k4v21m Ověření propojení MC před ověřením subu

--- app/Modules/ClientModule/Component/Connection/Twitch/TwitchConnection.php
+++ app/Modules/ClientModule/Component/Connection/Twitch/TwitchConnection.php
@@ -3,6 +3,7 @@
 namespace App\Modules\ClientModule\Component\Connection\Twitch;
 
 use App\Enum\EFlashMessageType;
+use App\Modules\ApiModule\Model\User\Connector\MinecraftUserConnectFacade;
 use App\Modules\ApiModule\Model\User\Connector\TwitchUserConnectFacade;
 use App\Modules\ClientModule\Component\Connection\BaseConnection;
 use App\Repository\Primary\UserRepository;
@@ -17,10 +18,16 @@ class TwitchConnection extends BaseConnection
     public function __construct(
         ?int $userId,
         private readonly TwitchUserConnectFacade $twitchUserConnectFacade,
-        private readonly UserRepository $userRepository,
+        private readonly MinecraftUserConnectFacade $minecraftUserConnectFacade,
     )
     {
         parent::__construct($userId, $twitchUserConnectFacade);
+    }
+
+    public function render(): void
+    {
+        $this->template->minecraftConnected = $this->minecraftUserConnectFacade->isConnected($this->userId);
+        parent::render();
     }
 
     /**
@@ -37,24 +44,28 @@ class TwitchConnection extends BaseConnection
      */
     public function handleVerifySubscription(): void
     {
-        try {
-            $this->twitchUserConnectFacade->refreshSubscriptionForUser($this->presenter->user->id);
-            $this->presenter->flashMessage('Odběr byl úspěšně ověřen. Můžeš jít hrát na Subserver!', EFlashMessageType::SUCCESS);
-        } catch (GuzzleException $exception) {
-            $this->presenter->flashMessage('Nepodařilo se ověřit odběr.', EFlashMessageType::ERROR);
-            Debugger::log($exception, 'twitch');
-        } catch (Throwable $exception) {
-            $this->presenter->flashMessage('Při zpracování požadavku nastala neznámá chyba.', EFlashMessageType::ERROR);
-            Debugger::log($exception, 'twitch');
+
+        if (!$this->minecraftUserConnectFacade->isConnected($this->userId)) {
+            $this->presenter->flashMessage('Nejprve se musíš připojit na Minecraftem!', EFlashMessageType::ERROR);
+        } else {
+            try {
+                $this->twitchUserConnectFacade->refreshSubscriptionForUser($this->presenter->user->id);
+                $this->presenter->flashMessage('Odběr byl úspěšně ověřen. Můžeš jít hrát na Subserver!', EFlashMessageType::SUCCESS);
+            } catch (GuzzleException $exception) {
+                $this->presenter->flashMessage('Nepodařilo se ověřit odběr.', EFlashMessageType::ERROR);
+                if ($exception->getCode() !== 404) {
+                    Debugger::log($exception, 'twitch');
+                }
+            } catch (Throwable $exception) {
+                $this->presenter->flashMessage('Při zpracování požadavku nastala neznámá chyba.', EFlashMessageType::ERROR);
+                Debugger::log($exception, 'twitch');
+            }
         }
 
-        if ($this->presenter->isAjax())
-        {
+        if ($this->presenter->isAjax()) {
             $this->redrawControl();
             $this->presenter->redrawControl();
-        }
-        else
-        {
+        } else {
             $this->presenter->redirect('this');
         }
     }
